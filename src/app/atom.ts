c8377a272ef585e47eeb594afc230dcf0c1fac20
MarkoBitencCode <marko.bitenc@codelivery.team>
2024-02-20 21:15:01 +0100

    Add location and skeleton comp

---
+++ src/app/atom.ts
@@ -0,0 +1,5 @@
+import { atom } from "jotai";
+
+//
+export const placeAtom = atom("Slovenia");
+export const loadingCityAtom = atom(false);
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,17 +4,22 @@ import React, { useState } from "react";
 import { MdMyLocation, MdOutlineLocationOn, MdWbSunny } from "react-icons/md";
 import SearchBox from "./SearchBox";
 import axios from "axios";
+import { useAtom } from "jotai";
+import { loadingCityAtom, placeAtom } from "@/app/atom";
 
 type Props = {};
 
 const API_KEY = process.env.NEXT_PUBLIC_WEATHER_KEY;
 
-export default function Navbar({}: Props) {
+export default function Navbar({ location }: Props) {
   const [city, setCity] = useState("");
   const [error, setError] = useState("");
   //
   const [suggestions, setSuggestions] = useState<string[]>([]);
   const [showSuggestions, setShowSuggestions] = useState(false);
+
+  const [place, setPlace] = useAtom(placeAtom);
+  const [, setloadingCity] = useAtom(loadingCityAtom);
 
   async function handleInputChange(value: string) {
     setCity(value);
@@ -37,6 +42,27 @@ export default function Navbar({}: Props) {
     }
   }
 
+  function handleSuggestionClick(value: string) {
+    setCity(value);
+    setShowSuggestions(false);
+  }
+
+  function handleSubmitSearch(e: React.FormEvent<HTMLFormElement>) {
+    setloadingCity(true);
+    e.preventDefault();
+    if (suggestions.length == 0) {
+      setError("Location not found.");
+      setloadingCity(false);
+    } else {
+      setError("");
+      setTimeout(() => {
+        setloadingCity(false);
+        setPlace(city);
+        setShowSuggestions(false);
+      }, 500);
+    }
+  }
+
   return (
     <nav className='shadoow-sm sticky top-0 left-0 z-50 bg-white'>
       <div className='h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto'>
@@ -47,13 +73,13 @@ export default function Navbar({}: Props) {
         <section className='flex gap-2 items-center'>
           <MdMyLocation className='text-2xl text-gray-600 hover:opacity-80 cursor-pointer' />
           <MdOutlineLocationOn className='text-3xl' />
-          <p className='text-slate-900/80 text-sm'></p>
+          <p className='text-slate-900/80 text-sm'>{location}</p>
           <div className='relative'>
             {" "}
             <SearchBox
               value={city}
+              onSubmit={handleSubmitSearch}
               onChange={(e) => handleInputChange(e.target.value)}
-              onSubmit={undefined}
             />
             <SuggestionBox
               {...{
