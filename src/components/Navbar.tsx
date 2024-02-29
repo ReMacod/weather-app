a6a606c31c153fa356bd0be05a5b2f8b7f14cd5d
MarkoBitencCode <marko.bitenc@codelivery.team>
2024-02-29 21:22:09 +0100

    fix show error in input

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { MdMyLocation, MdOutlineLocationOn, MdWbSunny } from "react-icons/md";
 import SearchBox from "./SearchBox";
 import axios from "axios";
@@ -25,6 +25,7 @@ export default function Navbar({ location }: Props) {
     setCity(value);
     if (value.length >= 3) {
       try {
+        setError("");
         const response = await axios.get(
           `https://api.openweathermap.org/data/2.5/find?q=${value}&APPID=${API_KEY}`
         );
@@ -32,7 +33,6 @@ export default function Navbar({ location }: Props) {
         console.log("suggestions", suggestions);
         console.log("response", response.data.list);
         setSuggestions(suggestions);
-        setError("");
         setShowSuggestions(true);
       } catch (error) {
         setSuggestions([]);
@@ -52,7 +52,7 @@ export default function Navbar({ location }: Props) {
   function handleSubmitSearch(e: React.FormEvent<HTMLFormElement>) {
     setloadingCity(true);
     e.preventDefault();
-    if (suggestions.length == 0) {
+    if (suggestions.length === 0) {
       setError("Location not found.");
       setloadingCity(false);
     } else {
@@ -108,13 +108,22 @@ function SuggestionBox({
   handleSuggestionClick: (item: string) => void;
   error: string;
 }) {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    if (error && suggestions.length < 1) {
+      setShow(true);
+      setTimeout(() => {
+        setShow(false);
+      }, 2000);
+    }
+  }, [error, suggestions]);
+  console.log("error", error);
   return (
     <>
-      {((showSuggestions && suggestions.length > 1) || error) && (
-        <ul className='mb-4 bg-white absoulte border top-[44px] left-0 border-gray-300 rounded-md min-w-[200px] flex flex-col gap-1 py-2 px-2'>
-          {error && suggestions.length < 1 && (
-            <li className='text-red-500 p-1'>{error}</li>
-          )}
+      {((showSuggestions && suggestions.length > 0) || show) && (
+        <ul className='mb-4 bg-white absolute border top-[44px] left-0 border-gray-300 rounded-md min-w-[200px] flex flex-col gap-1 py-2 px-2'>
+          {show && <li className='text-red-500 p-1'>{error}</li>}
           {suggestions.map((item, i) => (
             <li
               key={i}
